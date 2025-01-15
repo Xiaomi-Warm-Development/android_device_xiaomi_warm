756fbaaab58f7ca6b9ca67c64e65b9ff00e5deeb
Guidix <guidixm@gmail.com>
2025-01-15 14:58:05 +0200

    warm: Build FastCharge instead of lineage fastcharge
    Change-Id: Ib66603768e6a3b24191f535fd5b7b4a54946804e
    Co-Authored-By: cyberknight777 <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -215,9 +215,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
+# FastCharge
 PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi
+    FastCharge
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -470,6 +470,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayPeridotRedmi \
     SystemUIOverlayPeridot \
     TelephonyOverlayPeridot \
+    FastChargePeridot \
     WifiOverlayPeridot \
     WifiOverlayPeridotPOCO \
     WifiOverlayPeridotRedmi
