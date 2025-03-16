9bd240fa8193c7b9d2b275b11127a67f9e4ca01b
raghavt20 <raghavt20@aospa.co>
2025-03-16 15:20:46 +0530

    warm: Use common hardware/qcom-caf/bootctrl aidl service
    Change-Id: I88fa51b112a36766a997bdc9b72072c36ba7edc2
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,6 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
 
 # Recovery
-$(call soong_config_set, ufsbsg, ufsframework, bsg)
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
