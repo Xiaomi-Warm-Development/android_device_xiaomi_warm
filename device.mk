4f2e05d82f4dca7b52ed3a10a165d7a71e931dcb
kenway214 <kenway214@outlook.com>
2025-04-24 11:00:16 +0530

    warm: Drop OMX Dependencies for latest sources
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- device.mk
+++ device.mk
@@ -393,7 +393,6 @@ endif
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.2.vendor \
-    libOmxCore \
     libavservices_minijail \
     libavservices_minijail_vendor \
     libcodec2_hidl@1.0.vendor \
@@ -402,8 +401,7 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl_shim \
     libcodec2_soft_common.vendor \
     libcodec2_shim \
-    libcodec2_vndk.vendor \
-    libmm-omxcore
+    libcodec2_vndk.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.media.bufferpool2-V1-ndk.vendor
