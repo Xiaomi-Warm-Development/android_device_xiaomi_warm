ede65e269822b4b0399aecd8282378d7e3b6793d
ZaraKinYu <zarakinyu67@gmail.com>
2025-11-13 16:06:10 +0000

    warm: initial system_ext properties
    Signed-off-by: ZaraKinYu <zarakinyu67@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,6 +164,7 @@ TARGET_BOARD_PLATFORM := pitti
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/props/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/props/system_ext.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
 
 # Recovery
