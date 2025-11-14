72e00acd7c66f119ab513ff6b51d0de1f9528c89
ZaraKinYu <zarakinyu67@gmail.com>
2025-11-14 16:34:33 +0530

    warm: nuke firmare bootloader files

--- extract-files.py
+++ extract-files.py
@@ -93,8 +93,7 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    check_elf=True,
-    add_firmware_proprietary_file=True
+    check_elf=True
 )
 
 if __name__ == '__main__':
